975b98f9abcd21390deb420a17c02957fe765637
alstjq8251 <98382954+alstjq8251@users.noreply.github.com>
2022-05-12 16:22:49 +0900

    Update Main.java

--- 문제풀이/week5/민섭/boj_4673/Main.java
+++ 문제풀이/week5/민섭/boj_4673/Main.java
@@ -1,4 +1,28 @@
 package 문제풀이.week5.민섭.boj_4673;
 
 public class Main {
+    public static void main(String[] args) {
+ 
+        boolean [] a = new boolean[10001];     
+ 
+        for (int i = 1; i<10001; i++) {
+            int n = d(i);
+ 
+            if (n < 10001)             
+                a[n] = true;
+        }
+            for (int i = 1; i < 10001; i++) {   
+                if (a[i]==false) 
+                    System.out.println(i);
+            }
+ 
+    }
+    public static int d(int n){
+        int sum = n;
+        while (n != 0){ 
+            sum += n%10;                   
+            n /= 10;             
+        }
+        return sum;                             
+    }
 }
